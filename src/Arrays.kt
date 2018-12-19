9ab0a38aaf0cbb767628294b1e9e6996e904403c
olszewskimichal <olszewskimichal@outlook.com>
2018-12-19 21:11:31 +0100

    arrays in kotlin

---
+++ src/Arrays.kt
@@ -0,0 +1,17 @@
+class Arrays {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val numbers = arrayOf(1, 2, 3, 4, 5)
+            val animals = arrayOf("Cat", "Dog", "Lion", "Tiger")
+
+            val firstElement = numbers[0]
+            val lastElement = numbers[numbers.size - 1]
+            println("$firstElement $lastElement")
+
+            for (animal in animals) {
+                println(animal)
+            }
+        }
+    }
+}
