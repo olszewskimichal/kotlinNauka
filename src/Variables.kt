3350c3765ebef88ce73a9ae5fba616b75e4f41e6
olszewskimichal <olszewskimichal@outlook.com>
2018-12-19 21:10:48 +0100

    using variable and values in Kotlin

---
+++ src/Variables.kt
@@ -0,0 +1,21 @@
+class Variables {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val name = "Bill Gates"
+            //name = "Satoshi Nakamoto"	// Error: Val cannot be reassigned
+
+            val country: String = "India"
+
+            val greeting = "Hello, World"  // type inferred as `String`
+            val year = 2018                // type inferred as `Int`
+
+            // Explicitly defining the type of variables
+            val greeting2: String = "Hello, World"
+            val year2: Int = 2018
+
+            println("$name $country $greeting $greeting2 $year $year2")
+        }
+    }
+}
