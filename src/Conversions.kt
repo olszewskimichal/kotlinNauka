b413f1dd3806f5851badbd60a2b5324400a84782
olszewskimichal <olszewskimichal@outlook.com>
2018-12-19 21:11:59 +0100

    conversion in mongo

---
+++ src/Conversions.kt
@@ -0,0 +1,16 @@
+class Conversions {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val myInt = 100
+            val myLong = myInt.toLong()   // Explicitly converting 'Int' to 'Long'
+
+            val doubleValue = 176.80
+            val intValue = doubleValue.toInt()  // 176
+
+            val str = "1000"
+            val intValueFromString = str.toInt()
+            println("$myLong $intValue $intValueFromString")
+        }
+    }
+}
