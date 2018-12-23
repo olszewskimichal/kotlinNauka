cb9125a5942980b541da92035f879daed8f3c296
olszewskimichal <olszewskimichal@outlook.com>
2018-12-23 12:56:15 +0100

    Destructuring declarations in Kotlin

--- src/classes/DustructiuringDeclarations.kt
+++ src/classes/DustructiuringDeclarations.kt
@@ -1,2 +1,19 @@
 package classes
 
+
+data class PersonGOT(var firstName: String, var lastName: String,var age : Int)
+
+fun main(args: Array<String>) {
+    val person = PersonGOT("Jon", "Snow", 20)
+    val(firstname, lastName, age) = person
+    println(firstname)
+    println(lastName)
+    println(age)
+
+    val firstname2 = person.component1()
+    val lastName2 = person.component2()
+    val age2 = person.component3()
+    println(firstname2)
+    println(lastName2)
+    println(age2)
+}
