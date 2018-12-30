6a864d375d2a35017b36e6521b7d7ed292350206
olszewskimichal <olszewskimichal@outlook.com>
2018-12-30 21:14:15 +0100

    petClinic initialization

---
+++ petClinic/src/test/kotlin/com/example/petclinic/PetClinicApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.example.petclinic
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
